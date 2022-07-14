efd38c312934d011d4649df26245feea0ece873d
sh-dinho <sadiqy93@gmail.com>
2022-07-13 20:12:02 -0600

    Updating

--- date.py
+++ date.py
@@ -7,6 +7,5 @@ user_input = int(input("Enter a number: "))
 today = dt.date.today()
 date = today + dt.timedelta(days=user_input)
 #display the date in this format: Saturday, February 06, 2021 (Saturday, February 06, 2021)
-format  = "%A, %B %d, %Y"   # %A is the day of the week, %B is the month, %d is the day, %Y is the year
-print(date.strftime(format))
-
+print(date.strftime("%A, %B %d, %Y"))
+#display the date in this format: Saturday, February 06, 2021 (Saturday, February 06, 2021)
